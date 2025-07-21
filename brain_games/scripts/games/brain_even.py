e5058f29a5001a9481f16297657f0e72d6d82965
sirnapster88 <sir.napster@yandex.ru>
2025-07-21 14:14:03 +0300

    update brain-even

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -1,5 +1,5 @@
 from random import randint
-from brain_games.scripts.greeting import greet, results
+from brain_games.scripts.greeting import greet
 from brain_games.scripts.logic import check
 import prompt
 
