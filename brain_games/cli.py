29877350d6fdb046e16fcebcd99c3a213b4f2229
sirnapster88 <sir.napster@yandex.ru>
2025-07-21 11:59:23 +0300

    update brain-games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,5 +3,4 @@ import prompt
 
 def welcome_user():
     name = prompt.string('May I have Your name? ')
-    print('Welcome to the Brain Games!')
     print(f"Hello, {name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,6 +2,7 @@ from .. import cli
 
 
 def main():
+    print('Welcome to the Brain Games!')
     cli.welcome_user()
 
 
