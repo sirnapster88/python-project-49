5efd191c6b87890723462232ce91f6ae6632875a
sirnapster88 <sir.napster@yandex.ru>
2025-07-21 14:53:04 +0300

    update all linter bugs

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -10,7 +10,7 @@ def nod(num1, num2):
     if num2 == 0:
         return num1
     while num2 != 0:
-       num1, num2 = num2, num1 % num2
+        num1, num2 = num2, num1 % num2
     return num1
 
 
