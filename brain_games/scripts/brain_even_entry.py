e1ca0c8b95cf06731ce3da3eaa01c274707c5c56
sirnapster88 <sir.napster@yandex.ru>
2025-07-22 09:48:03 +0300

    fix brain_even_entry

--- brain_games/scripts/brain_even_entry.py
+++ brain_games/scripts/brain_even_entry.py
@@ -1,11 +1,11 @@
 from brain_games.games import brain_even
 from brain_games.scripts import engine
 
-game_objective = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+game_obj = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 
 def main():
-    engine.engine_logic(brain_even.is_even, game_objective)
+    engine.engine_logic(brain_even.is_even, game_obj)
 
 
 if __name__ == '__main__':
